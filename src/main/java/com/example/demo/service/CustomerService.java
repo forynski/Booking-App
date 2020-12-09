c5c45e1f29fe213b0259ab027dce801e37f86a06
Robert Forynski <forynski@gmail.com>
2020-12-09 11:04:12 +0000

    cleanup

--- src/main/java/com/example/demo/service/CustomerService.java
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -1,19 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Customer;
-
-import java.util.List;
-
 public interface CustomerService {
 
-    Customer createNewCustomer(Customer customer);
-
-    Customer getCustomerById(Long id);
-
-    List<Customer> getAllCustomers(Integer page, Integer size);
-
-    Customer updateCustomerById(Long id, Customer customer);
-
-    boolean removeCustomerById(Long id);
 
 }
