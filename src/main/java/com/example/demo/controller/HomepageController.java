86d79bd676ff14e6be094f0e3e73c75c6f5125e9
Robert Forynski <forynski@gmail.com>
2021-01-11 12:41:49 +0000

    basic search engine added to homepage

--- src/main/java/com/example/demo/controller/HomepageController.java
+++ src/main/java/com/example/demo/controller/HomepageController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Booking;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Objects;
 
@@ -13,14 +16,27 @@ import java.util.Objects;
 public class HomepageController {
 
     @GetMapping("/")
-    public String homePage(ModelMap modelMap, @AuthenticationPrincipal org.springframework.security.core.userdetails.User authenticationUser) {
+    public String homePage(ModelMap modelMap, Model model, @AuthenticationPrincipal org.springframework.security.core.userdetails.User authenticationUser) {
         boolean isUserLogged = Objects.nonNull(authenticationUser);
         modelMap.addAttribute("isUserLogged", isUserLogged);
+        model.addAttribute("booking", new Booking());
         if (isUserLogged) {
             boolean isAuthorizedUserAdmin = authenticationUser.getAuthorities().stream().anyMatch(grantedAuthority ->
                     grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
             modelMap.addAttribute("isAuthorizedUserAdmin", isAuthorizedUserAdmin);
         }
         return "homepage";
+    }
+
+//    @GetMapping("/")
+//    public String main(Model model) {
+//        model.addAttribute("booking", new Booking());
+//        return "homepage";
+//    }
+
+    @PostMapping("/")
+    public String save(Booking booking, Model model) {
+        model.addAttribute("booking", booking);
+        return "test";
     }
 }
