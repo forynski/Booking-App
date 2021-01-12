1448843dc0280a4383c8ca1bb91526b886e53265
Robert Forynski <forynski@gmail.com>
2021-01-12 19:58:24 +0000

    view updates, CMS for customer management

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Customer;
 import com.example.demo.service.CustomerService;
+import org.dom4j.rule.Mode;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class CustomerController {
@@ -37,16 +39,31 @@ public class CustomerController {
     }
 
     @GetMapping(path = "/customer/add")
-    public String showCustomerAdd(ModelMap modelMap) {
+    public String showCustomerAdd(ModelMap modelMap, @AuthenticationPrincipal org.springframework.security.core.userdetails.User authenticationUser) {
         modelMap.addAttribute("customer", new Customer());
-//        return "customer-add";
-        return null;
+
+        boolean isUserLogged = Objects.nonNull(authenticationUser);
+        modelMap.addAttribute("isUserLogged", isUserLogged);
+        if (isUserLogged) {
+            boolean isAuthorizedUser = authenticationUser.getAuthorities().stream().anyMatch(grantedAuthority ->
+                    grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
+            modelMap.addAttribute("isAuthorizedUserAdmin", isAuthorizedUser);
+        }
+        return "customer-add";
     }
 
     @PostMapping(path = "/customer/add")
-    public String addCustomer(@Valid @ModelAttribute("customer") Customer customer) {
+    public String addCustomer(@Valid @ModelAttribute("customer") Customer customer, ModelMap modelMap, @AuthenticationPrincipal org.springframework.security.core.userdetails.User authenticationUser) {
+
+        boolean isUserLogged = Objects.nonNull(authenticationUser);
+        modelMap.addAttribute("isUserLogged", isUserLogged);
+        if (isUserLogged) {
+            boolean isAuthorizedUser = authenticationUser.getAuthorities().stream().anyMatch(grantedAuthority ->
+                    grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
+            modelMap.addAttribute("isAuthorizedUserAdmin", isAuthorizedUser);
+        }
         customerService.createNewCustomer(customer);
-        return "redirect:/";
+        return "redirect:/customer";
     }
 
 }
