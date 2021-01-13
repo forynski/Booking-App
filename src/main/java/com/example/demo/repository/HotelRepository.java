9ff1f003c4a1061ad79357659fff728af3b5eefc
Robert Forynski <forynski@gmail.com>
2021-01-13 00:57:58 +0000

    minor visual updates

--- src/main/java/com/example/demo/repository/HotelRepository.java
+++ src/main/java/com/example/demo/repository/HotelRepository.java
@@ -2,9 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
+
 
 }
